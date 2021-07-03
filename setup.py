de147c63cdfa1ae1a165a6d4e0ffd0bc8ea78a1e
JavideSs <javimelladoo@gmail.com>
2021-07-03 21:57:50 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-#Build .exe -> cmd>> python setup.py build_exe
-#Build .exe with installer -> cmd>> python setup.py bdist_msi(win)/bdist_dmg(mac)
-import os, sys
+#Build>> python setup.py build_exe
+import os, sys, shutil
 from cx_Freeze import setup, Executable
 
-os.environ["TCL_LIBRARY"] = os.path.join(sys.exec_prefix, "tcl", "tcl8.6")
-os.environ["TK_LIBRARY"] = os.path.join(sys.exec_prefix, "tcl", "tk8.6")
+with open("README.md", "r") as readme_f:
+    readme = readme_f.read()
+
+icon_path = os.path.join("data", "images", "images_files", "CD", "IconExplorer.ico")
 
 setup(
     name="Reproc",
     version="1.0.0",
-    description="Reproc",
-    long_description="Reproc",
+    description="Reproc - Local Music Player",
+    long_description=readme,
     long_description_content_type="text/markdown",
+    keywords="reproc local music player multiplaylists python tkinter tcl pygame.mixer tinytag",
 
-    url="https://github.com/JavideSs/Reproc",
+    url="https://github.com/JavideSs/reproc",
 
     author="Javier Mellado Sánchez",
     author_email="javimelladoo@gmail.com",
@@ -24,19 +26,26 @@ setup(
     executables = [Executable (
         script="reproc.py",
         target_name="Reproc",
-        #icon=os.path.join("data", "images", "images_files", "CD" "IconExplorer.ico"),
+        icon=icon_path,
         base="Win32GUI" if sys.platform == "win32" else None
     )],
 
     options = {"build_exe": {
         "optimize": 1,
         "include_msvcr": True,
-        "packages": ["pygame", "PIL", "tinytag"],
-        "includes": ["tkinter", "pygame", "PIL", "tinytag"],
-        "excludes" : [],
-        #"include_files": [
-        #    os.path.join(sys.path[2], "tcl86t.dll"),
-        #    os.path.join(sys.path[2], "tk86t.dll")
-        #]
+        #"packages": [],
+        #"includes": [],
+        #"excludes" : [],
     }}
 )
+
+#Copy data and themes
+with os.scandir("build") as it:
+    for build in it:
+        path_themes = os.path.join(build.path, "customTk", "ttk_themes")
+        path_data = os.path.join(build.path, "data")
+        path_locale = os.path.join(path_data, "locale")
+
+        shutil.copytree(os.path.join("customTk", "ttk_themes"), path_themes)
+        shutil.copytree(os.path.join("data", "locale"), path_locale)
+        shutil.copy2(os.path.join("data", "user_config.json"), path_data)
